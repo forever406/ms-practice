9cbe345f289ce92d930d58ed7a6f19e46498ad5e
vkwk-matebook-linux <vikingwarlock@163.com>
2019-05-15 00:15:00 +0800

    account rpc test is done

--- TestService/AccountTest/account_rpc_test.py
+++ TestService/AccountTest/account_rpc_test.py
@@ -1,19 +1,32 @@
 import grpc
 import service_pb2
 import service_pb2_grpc
+import uuid
 
 
-def signup(username, password):
-    pass
+def signup(stub, username, password):
+    signup_result = stub.SignupNewUser(service_pb2.SignupMessage(username=username, password=password))
+    return signup_result
 
 
-def login(username, password):
-    pass
+def login(stub, username, password):
+    login_result = stub.LoginUser(service_pb2.LoginMessage(username=username, password=password))
+    return login_result
 
 
 def run_test():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = service_pb2_grpc.AccountServiceStub(channel)
+        username = str(uuid.uuid4())
+        password = str(uuid.uuid4())
+        signup_response = signup(stub, username, password)
+        print signup_response.message
+        signup_fail_response = signup(stub, username, str(uuid.uuid4()))
+        print signup_fail_response.message
+        login_response = login(stub, username, password)
+        print login_response.message
+        ogin_fail_response = login(stub, username, str(uuid.uuid4()))
+        print ogin_fail_response.message
 
 
 if __name__ == '__main__':
