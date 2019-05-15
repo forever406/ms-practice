e30e0a6c294a6e961eefe86bbdb8266d32b65676
Ong <qzj@macAir.com>
2019-05-15 17:17:32 +0800

    express run

---
+++ GatewayService/index.js
@@ -0,0 +1,19 @@
+const http=require('http');
+const express=require('express');
+const logger=require('morgan');
+const app=express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+
+app.use('/native',require('./router/native'));
+app.use('/grpc',require('./router/grpc'));
+
+
+const server=http.createServer(app);
+server.listen(3000,()=>{
+    console.log('Nodejs running on port 3000');
+});
