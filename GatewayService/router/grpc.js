deadb360412e895bc4976f2fdcfc57042b8f9304
Ong <qzj@macAir.com>
2019-05-15 18:11:47 +0800

    grpc native done

--- GatewayService/router/grpc.js
+++ GatewayService/router/grpc.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const PROTO_PATH = __dirname + '/../../AccountMicroService/service.proto';
+const grpc=require('grpc');
+const protoLoader=require('@grpc/proto-loader');
+const packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    });
+const account_proto=grpc.loadPackageDefinition(packageDefinition).accountservice;
+const HOST='';
+let client=new account_proto.AccountService(HOST,
+    grpc.credentials.createInsecure());
+router.post('/login', (req, res, next)=> {
+    const body=req.body;
+    let result=client.SignupNewUser({username:body.username,
+        password:body.password},(err,response)=>{
+        res.json({msg:response.message})
+    });
+});
 
-
-router.get('/', (req, res, next)=> {
-    res.json({msg:'hello from grpc'})
+router.post('/login',(req, res, next)=> {
+    const body=req.body;
+    client.SignupNewUser({username:body.username,
+        password:body.password},(err, response)=>{
+        res.json({msg:response.message})
+    };
 });
 
 module.exports=router;
--- GatewayService/router/native.js
+++ GatewayService/router/native.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const {Client}=require('pg');
 const router = express.Router();
-
-
-router.get('/', (req, res, next)=> {
-    res.json({msg:'hello from native'})
+const client = new Client({
+    host: 'db',
+    port: 5334,
+    user: '',
+    password: '',
 });
 
+router.post('/add', async(req, res, next)=> {
+    let body=req.body;
+    await client.connect();
+    let dbResult=await client.query('INSERT INTO user_test_table(username, password) VALUES($1, $2)', [body.username, body.password]);
+    client.end((err) => {
+        console.log('client has disconnected');
+        if (err) {
+            console.log('error during disconnection', err.stack)
+        }
+    });
+    res.json({msg:dbResult})
+});
+router.post('/login', async(req, res, next)=> {
+    let body=req.body;
+    await client.connect();
+    let dbResult=await client.query('select username, password from person where username = $1', body.username);
+    (body.password===dbResult.row[0].password)? res.json({msg:'log in succeed'}):res.json({msg:'log in fail'})
+});
 module.exports=router;
