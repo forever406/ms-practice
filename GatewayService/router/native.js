0fede9db2815853556487a33ea1ffbbe1cf97f0f
Ong <qzj@macAir.com>
2019-05-16 17:28:50 +0800

    using pool to construct client

--- GatewayService/router/native.js
+++ GatewayService/router/native.js
@@ -1,20 +1,23 @@
 const express = require('express');
-const {Client}=require('pg');
+const {Client,Pool}=require('pg');
 const router = express.Router();
-const client =new Client({
-      host: 'db',
-      port: 5432,
-      database: 'user_test_table',
-      user: 'postgres',
-      password: '',
+const pool =new Pool({
+    host: 'db',
+    port: 5432,
+    database: 'user_test_table',
+    user: 'postgres',
+    password: '',
+    max: 100,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
   });
-client.connect();
 client.query('CREATE EXTENSION IF NOT EXISTS "pgcrypto";');
 
 
 router.post('/add', async(req, res, next)=> {
     let body=req.body;
     console.log(body);
+    const client = await pool.connect();
     let dbResult=await client.query('SELECT * FROM account WHERE username = ($1);',[body.username]);
     if (dbResult.rows[0]){
         res.json({
@@ -22,23 +25,20 @@ router.post('/add', async(req, res, next)=> {
             msg:'account already exists'
         });
     }else{
-        client.query("INSERT INTO account(username, password,userid) VALUES('"+
+       let result=await client.query("INSERT INTO account(username, password,userid) VALUES('"+
             body.username+ "', '"+
             body.password+ "',"+"gen_random_uuid());"
-        ).then(
-            (result)=> {
-                res.json({
-                    status: 0,
-                    msg: 'add account succeed'
-                });
-            }
-        ).catch((err)=>{
-            console.log(err);
+        )
+        res.json({
+            status: 0,
+            msg: 'add account succeed'
         });
     }
+    client.release(true)
 });
 router.post('/login', async(req, res, next)=> {
     let body=req.body;
+    const client = await pool.connect();
     let dbResult= await client.query('SELECT * FROM account WHERE username = ($1);',[body.username]);
     dbResult.rows[0].password&&(body.password===dbResult.rows[0].password)? res.json({
         msg:'log in succeed',
@@ -47,5 +47,6 @@ router.post('/login', async(req, res, next)=> {
     }):res.json({
         status:-1,
         msg:'log in fail'});
+    client.release(true)
 });
 module.exports=router;
