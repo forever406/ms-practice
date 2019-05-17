8be106b5f63da7f3a314b19c83704576286da6cd
Ong <qzj@macAir.com>
2019-05-17 14:53:13 +0800

    load balance 3

--- GatewayService/router/grpc.js
+++ GatewayService/router/grpc.js
@@ -14,21 +14,33 @@ const packageDefinition = protoLoader.loadSync(
 const account_proto=grpc.loadPackageDefinition(packageDefinition).accountservice;
 const HOST='account:50051';
 const HOST1='account1:50051';
+const HOST2='account1:50051';
 let client=new account_proto.AccountService(HOST,
     grpc.credentials.createInsecure());
 let client1=new account_proto.AccountService(HOST1,
     grpc.credentials.createInsecure());
+let client2=new account_proto.AccountService(HOST1,
+    grpc.credentials.createInsecure());
 router.post('/add', (req, res, next)=> {
     const body=req.body;
     console.log(body);
-    Math.random()<0.5?
-    client.SignupNewUser({username:body.username,
-        password:body.password},(err,response)=>{
-        res.json({msg:response.message})
-    }):client1.SignupNewUser({username:body.username,
+    let flag=Math.random();
+    if(flag<=0.33){
+        client.SignupNewUser({username:body.username,
             password:body.password},(err,response)=>{
             res.json({msg:response.message})
-    });
+        })
+    }else if(flag>0.67){
+        client1.SignupNewUser({username:body.username,
+            password:body.password},(err,response)=>{
+            res.json({msg:response.message})
+        });
+    }else {
+        client2.SignupNewUser({username:body.username,
+            password:body.password},(err,response)=>{
+            res.json({msg:response.message})
+        });
+    }
 });
 
 router.post('/login',(req, res, next)=> {
