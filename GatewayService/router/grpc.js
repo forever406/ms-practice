2dd347218593ba572c7ccc7c1e12600449e59237
Ong <qzj@macAir.com>
2019-05-17 14:44:04 +0800

    load balance

--- GatewayService/router/grpc.js
+++ GatewayService/router/grpc.js
@@ -13,14 +13,21 @@ const packageDefinition = protoLoader.loadSync(
     });
 const account_proto=grpc.loadPackageDefinition(packageDefinition).accountservice;
 const HOST='account:50051';
+const HOST1='account1:50051';
 let client=new account_proto.AccountService(HOST,
+    grpc.credentials.createInsecure());
+let client1=new account_proto.AccountService(HOST1,
     grpc.credentials.createInsecure());
 router.post('/add', (req, res, next)=> {
     const body=req.body;
     console.log(body);
+    Math.random()<0.5?
     client.SignupNewUser({username:body.username,
         password:body.password},(err,response)=>{
         res.json({msg:response.message})
+    }):client1.SignupNewUser({username:body.username,
+            password:body.password},(err,response)=>{
+            res.json({msg:response.message})
     });
 });
 
