be85b4f49caf20a01c6a091d3282b460819b18c1
Ong <qzj@macAir.com>
2019-05-17 17:52:51 +0800

    prepare to use nginx

--- GatewayService/router/grpc.js
+++ GatewayService/router/grpc.js
@@ -13,34 +13,38 @@ const packageDefinition = protoLoader.loadSync(
     });
 const account_proto=grpc.loadPackageDefinition(packageDefinition).accountservice;
 const HOST='account:50051';
-const HOST1='account1:50051';
-const HOST2='account2:50051';
+// const HOST1='account1:50051';
+// const HOST2='account2:50051';
 let client=new account_proto.AccountService(HOST,
     grpc.credentials.createInsecure());
-let client1=new account_proto.AccountService(HOST1,
-    grpc.credentials.createInsecure());
-let client2=new account_proto.AccountService(HOST2,
-    grpc.credentials.createInsecure());
+// let client1=new account_proto.AccountService(HOST1,
+//     grpc.credentials.createInsecure());
+// let client2=new account_proto.AccountService(HOST2,
+//     grpc.credentials.createInsecure());
 router.post('/add', (req, res, next)=> {
     const body=req.body;
-    console.log(body);
-    let flag=Math.random();
-    if(flag<=0.33){
-        client.SignupNewUser({username:body.username,
-            password:body.password},(err,response)=>{
-            res.json({msg:response.message})
-        })
-    }else if(flag>0.67){
-        client1.SignupNewUser({username:body.username,
-            password:body.password},(err,response)=>{
-            res.json({msg:response.message})
-        });
-    }else {
-        client2.SignupNewUser({username:body.username,
-            password:body.password},(err,response)=>{
-            res.json({msg:response.message})
-        });
-    }
+    // console.log(body);
+    // let flag=Math.random();
+    // if(flag<=0.33){
+    //     client.SignupNewUser({username:body.username,
+    //         password:body.password},(err,response)=>{
+    //         res.json({msg:response.message})
+    //     })
+    // }else if(flag>0.67){
+    //     client1.SignupNewUser({username:body.username,
+    //         password:body.password},(err,response)=>{
+    //         res.json({msg:response.message})
+    //     });
+    // }else {
+    //     client2.SignupNewUser({username:body.username,
+    //         password:body.password},(err,response)=>{
+    //         res.json({msg:response.message})
+    //     });
+    // }
+    client.SignupNewUser({username:body.username,
+                password:body.password},(err,response)=>{
+                res.json({msg:response.message})
+            })
 });
 
 router.post('/login',(req, res, next)=> {
